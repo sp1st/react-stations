aafd12ba79c87726424e04c3eb5073d728ac64e0
28oso <28osoblanco2020@gmail.com>
2024-08-21 00:49:47 +0900

    clear: station3

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,13 @@ export const App = () => {
       <header>
         <h2>Dogアプリ</h2>
       </header>
+      <main>
+        <h2>犬の画像を表示するサイトです</h2>
+        <img 
+          src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg" 
+          alt="犬の画像" 
+        />
+      </main>
     </div>
   )
 }
