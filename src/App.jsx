129f4984036ca89a4d4d736df8517e448a58f9c9
28oso <28osoblanco2020@gmail.com>
2024-08-23 17:47:48 +0900

    clear: station6

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,24 @@ export const App = () => {
   const [dogUrl, setDogUrl] = useState(
     'https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg'
   )
+  
+  const dogAPI = async () => {
+    await fetch(
+      "https://dog.ceo/api/breeds/image/random", 
+      {method: "GET"}
+    ).then(response => {
+      if (response.ok){
+        // console.log(response)
+        return response.json()
+      }
+      throw new Error(`リクエストエラー ${response.status}`)
+    }).then(data => {
+      console.log(data.message)
+      setDogUrl(data.message)
+    }).catch(error => {
+      console.log(error)
+    })
+  }
 
   return (
     <div>
@@ -23,9 +41,7 @@ export const App = () => {
           src={dogUrl}
           alt="犬の画像" 
         />
-        <button onClick={
-          () => setDogUrl("https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg")
-        }>
+        <button onClick={() => dogAPI()}>
             更新
         </button>
       </main>
